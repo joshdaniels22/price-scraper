0516ec624f351d80a99acef714426aa98bb8a0c3
Joshua Daniels <joshuadaniels537@gmail.com>
2023-02-25 23:04:26 +0200

    none: added mvc folder structure

---
+++ controller/tests/example.spec.js
@@ -0,0 +1,23 @@
+const { test } = require('@playwright/test')
+const fs = require('fs')
+const cheerio = require("cheerio")
+const moment = require("moment")
+
+test('not a real rest', async ({ page }) => {
+  const url = 'https://www.takealot.com/lego-technic-app-controlled-top-gear-rally-car/PLID60538045'
+  await page.goto(url);
+  const html = await page.content()
+  const $ = cheerio.load(html)
+  
+  var currentDate = moment().format('DD-MM-YYYY H:mm');
+
+  $('.buybox-module_price_2YUFa', html).each(function () {
+    const price = $(this).text()
+    const rowEntry = price + " " + currentDate + '\n'
+
+
+    fs.appendFile('prices.txt', rowEntry, (err) => {
+      if (err) throw err;
+    })    
+  })
+});
